2f03a5664972a1e95e9a34862b602652d6ea4f1a
akariiijima <akabelle14@gmail.com>
2020-11-28 23:27:20 +0900

    属性変更

--- start.php
+++ start.php
@@ -14,21 +14,23 @@
 		これからファッション画像について<br>
 		以下の問いと印象評価をしていただきます。
 	</h3>
+	画像特徴<br>
+	#女性衣服 #着衣 #膝から首まで #白背景<br><br>
 	印象評価5段階5項目<br>
 	<table>
 		<tbody>
 			<tr><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-			<tr><td></td><td>「色彩」</td><td style="width:150px;">暗い/鈍い</td><td>⇄</td><td style="width:150px;">明るい/鮮やか</td><td></td></tr>
-			<tr><td></td><td>「立体感」</td><td style="width:150px;">平面的/フィット</td><td>⇄</td><td style="width:150px;">立体的/ルーズ</td><td></td></tr>
-			<tr><td></td><td>「正統性」</td><td style="width:150px;">高級/フォーマル</td><td>⇄</td><td style="width:150px;">質素/カジュアル</td><td></td></tr>
-			<tr><td></td><td>「穏健性」</td><td style="width:150px;">無個性/自然</td><td>⇄</td><td style="width:150px;">個性的/不自然</td><td></td></tr>
-			<tr><td></td><td>「装飾性」</td><td style="width:150px;">地味/シンプル</td><td>⇄</td><td style="width:150px;">派手/ゴージャス</td><td></td></tr>
+			<tr><td></td><td>「色彩」</td><td style="width:150px;">暗い</td><td>⇄</td><td style="width:150px;">明るい</td><td></td></tr>
+			<tr><td></td><td>「立体感」</td><td style="width:150px;">フィット</td><td>⇄</td><td style="width:150px;">ルーズ</td><td></td></tr>
+			<tr><td></td><td>「正統性」</td><td style="width:150px;">フォーマル</td><td>⇄</td><td style="width:150px;">カジュアル</td><td></td></tr>
+			<tr><td></td><td>「穏健性」</td><td style="width:150px;">日常的な</td><td>⇄</td><td style="width:150px;">非日常的な</td><td></td></tr>
+			<tr><td></td><td>「装飾性」</td><td style="width:150px;">シンプル</td><td>⇄</td><td style="width:150px;">ゴージャス</td><td></td></tr>
 			<tr><td></td><td></td><td></td><td></td><td></td><td></td></tr>
 		</tbody>
 	</table>
+	<br>[ 注意事項 ]<br>
 	<h3>
-		[ 注意事項 ]<br>
-		画像は合計1000枚あり、1枚ずつ表示されます。<br>
+		テスト画像は合計500枚あり、1枚ずつランダムに表示されます。<br>
 		各ページ下2つのボタンから次のアクションを決めてください。<br>
 	</h3>
 	<table>
@@ -40,9 +42,15 @@
 		</tbody>
 	</table>
 	<h3>
+		<font color="red">各画像の回答時間をログとして残しているため、<br>
+			途中休憩したい場合は「SEVE」で回答を終了してください。<br></font>
 		回答を再開したい場合はもう一度ログインしてください。<br>
 		一度回答した画像は再度表示されることはありません。<br><br>
 	</h3>
+	[ テスト環境を以下のように設定してから開始してください ]<br>
+	<div style="text-align:left;margin-left:35%;color:red">・自分のPCを使用<br>
+	・ブラウザはChromeを使用<br>
+	・目が疲れない程度の明るさに設定してユーザーテスト中は変更しない<br></div>
 	<form action="answer.php" method="post">
 		<input type="submit" value="スタート" class="btn-flat-border">
 	</form>
